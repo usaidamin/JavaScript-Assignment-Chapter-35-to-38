e665b94faf50100f51973dbdf375f7dc66771f59
Muhammad Usaid <usaidamin21@gmail.com>
2023-05-16 23:24:54 +0500

    JavaScript Chapter 35-38

--- app.js
+++ app.js
@@ -1,227 +1,227 @@
-// //Chapters 35 TO 38
+//Chapters 35 TO 38
 
-// function foo(){
-//     var newVar = Date();
-//     console.log(newVar);
-// }
-// foo();
+function foo(){
+    var newVar = Date();
+    console.log(newVar);
+}
+foo();
 
-// function greetUser() {
-//     var firstName = prompt("Enter Your First Name");
-//     var lastName = prompt("Enter Your Last Name");
-//     var fullName = firstName + lastName;
-//     alert("Assalam o Alaikum " + fullName);
-// }
+function greetUser() {
+    var firstName = prompt("Enter Your First Name");
+    var lastName = prompt("Enter Your Last Name");
+    var fullName = firstName + lastName;
+    alert("Assalam o Alaikum " + fullName);
+}
 
-// greetUser();
+greetUser();
 
-// function addNumber() {
-//     var num1 = +prompt("Enter First Number");
-//     var num2 = +prompt("Enter Second Number");
-//     var sumOf = num1 + num2;
-//     alert("Your Added Value Is " + sumOf);
-// }
+function addNumber() {
+    var num1 = +prompt("Enter First Number");
+    var num2 = +prompt("Enter Second Number");
+    var sumOf = num1 + num2;
+    alert("Your Added Value Is " + sumOf);
+}
 
-// addNumber();
+addNumber();
 
-// function calculator() {
-//     var num1 = +prompt("Enter First Number");
-//     var num2 = +prompt("Enter Second Number");
-//     var operator = prompt("Enter Your operator");
+function calculator() {
+    var num1 = +prompt("Enter First Number");
+    var num2 = +prompt("Enter Second Number");
+    var operator = prompt("Enter Your operator");
 
-//     if (operator === "+") {
-//         alert("Your Answer Is " + (num1 + num2));
-//     } else if (operator === "-") {
-//         alert("Your Answer Is " + (num1 - num2));
-//     }else if (operator === "/") {
-//         alert("Your Answer Is " + (num1 / num2));
-//     }else if (operator === "x") {
-//         alert("Your Answer Is " + (num1 * num2));
-//     }
-//     else{
-//         alert("Please Enter Correct Operator");
-//     }
+    if (operator === "+") {
+        alert("Your Answer Is " + (num1 + num2));
+    } else if (operator === "-") {
+        alert("Your Answer Is " + (num1 - num2));
+    }else if (operator === "/") {
+        alert("Your Answer Is " + (num1 / num2));
+    }else if (operator === "x") {
+        alert("Your Answer Is " + (num1 * num2));
+    }
+    else{
+        alert("Please Enter Correct Operator");
+    }
 
-// }
+}
 
-// calculator();
+calculator();
 
-// function square(number) {
-//     return number * number;
-// }
+function square(number) {
+    return number * number;
+}
 
-// var result = square(12);
-// console.log(result);
+var result = square(12);
+console.log(result);
 
-// function factorial(number) {
-//     if (number === 0 || number === 1) {
-//         return 1;
-//     } else {
-//         return number * factorial(number - 1);
-//     }
-// }
-// var result = factorial(5)
-// console.log(result);
+function factorial(number) {
+    if (number === 0 || number === 1) {
+        return 1;
+    } else {
+        return number * factorial(number - 1);
+    }
+}
+var result = factorial(5)
+console.log(result);
 
-// function startToEnd() {
-//     var startNum = +prompt("Enter Starting Number");
-//     var EndingNum = +prompt("Enter Ending Number");
-//     var counting = [];
-//     for (let i = startNum; i <= EndingNum; i++) {
-//         counting.push(i);
-//     }
-//    document.write(counting.join("" + "<br>"))
-// }
+function startToEnd() {
+    var startNum = +prompt("Enter Starting Number");
+    var EndingNum = +prompt("Enter Ending Number");
+    var counting = [];
+    for (let i = startNum; i <= EndingNum; i++) {
+        counting.push(i);
+    }
+   document.write(counting.join("" + "<br>"))
+}
 
-// startToEnd();
+startToEnd();
 
-// function calculateHypotenuse(base, perpendicular) {
+function calculateHypotenuse(base, perpendicular) {
 
-//     function calculateSquare(number) {
-//         return number * number
-//     }
+    function calculateSquare(number) {
+        return number * number
+    }
 
-//     var baseSquare = calculateSquare(base);
-//     var perpendicularSquare = calculateSquare(perpendicular);
+    var baseSquare = calculateSquare(base);
+    var perpendicularSquare = calculateSquare(perpendicular);
 
-//     var hypotenuseSquare = baseSquare + perpendicularSquare;
-//     var hypotenuse = Math.sqrt(hypotenuseSquare);
+    var hypotenuseSquare = baseSquare + perpendicularSquare;
+    var hypotenuse = Math.sqrt(hypotenuseSquare);
 
-//     return hypotenuse;
-// }
+    return hypotenuse;
+}
 
-// var results = calculateHypotenuse(3, 4);
-// console.log(results);
+var results = calculateHypotenuse(3, 4);
+console.log(results);
 
-// function calculateHypotenuse() {
-//     var base = +prompt("Enter Your Base For Solving");
-//     var perpendicular = +prompt("Enter Your Perpendicular For Solving");
+function calculateHypotenuse() {
+    var base = +prompt("Enter Your Base For Solving");
+    var perpendicular = +prompt("Enter Your Perpendicular For Solving");
 
-//     function calculateSquare(number) {
-//         return number * number;
-//     }
+    function calculateSquare(number) {
+        return number * number;
+    }
 
-//     var baseSquare = calculateSquare(base);
-//     var perpendicular = calculateSquare(perpendicular);
+    var baseSquare = calculateSquare(base);
+    var perpendicular = calculateSquare(perpendicular);
 
-//     var hypotenuseSquare = baseSquare + perpendicular;
-//     var hypotenuse = Math.sqrt(hypotenuseSquare);
+    var hypotenuseSquare = baseSquare + perpendicular;
+    var hypotenuse = Math.sqrt(hypotenuseSquare);
 
-//     return hypotenuse;
+    return hypotenuse;
 
-// }
+}
 
-// var results = calculateHypotenuse();
-// console.log(results);
+var results = calculateHypotenuse();
+console.log(results);
 
-// //Area For Rectangle Area
+//Area For Rectangle Area
 
-// function calculateArea(width, height) {
-//     var area = width * height;
-//     return area;
-// }
+function calculateArea(width, height) {
+    var area = width * height;
+    return area;
+}
 
-// var MyWidth = +prompt("Enter Your Width");
-// var MyHeight = +prompt("Enter Your Height");
-// var result = calculateArea(MyWidth, MyHeight);
-// alert("Your Value Is " + result);
+var MyWidth = +prompt("Enter Your Width");
+var MyHeight = +prompt("Enter Your Height");
+var result = calculateArea(MyWidth, MyHeight);
+alert("Your Value Is " + result);
 
-// function palindrome(str) {
+function palindrome(str) {
 
-//     var formattedStr = str.toLocaleLowerCase().replace(/[^a-z0-9]/g, '');
+    var formattedStr = str.toLocaleLowerCase().replace(/[^a-z0-9]/g, '');
 
-//     var reversedStr = formattedStr.split("").reverse("").join("");
+    var reversedStr = formattedStr.split("").reverse("").join("");
 
-//     if (formattedStr === reversedStr) {
-//         alert("Your Character Is Palindrome");
-//         return true;
-//     } else {
-//         alert("Your Character Is Not Palindrome")
-//         return false;
-//     }
-// }
+    if (formattedStr === reversedStr) {
+        alert("Your Character Is Palindrome");
+        return true;
+    } else {
+        alert("Your Character Is Not Palindrome")
+        return false;
+    }
+}
 
-// var string = prompt("Enter The Full Name That Is Palindrome");
-// var result = palindrome(string);
-// console.log(result);
+var string = prompt("Enter The Full Name That Is Palindrome");
+var result = palindrome(string);
+console.log(result);
 
-// function capitalizeFirstLetter(str) {
+function capitalizeFirstLetter(str) {
 
-//     const words = str.split(' ');
-//     const capitalizedWords = words.map(word => {
-//         const firstLetter = word.charAt(0).toUpperCase();
-//         const restOfWord = word.slice(1);
-//         return firstLetter + restOfWord;
-//     });
-//     const capitalizedString = capitalizedWords.join(' ');
-//     return capitalizedString;
-// }
+    const words = str.split(' ');
+    const capitalizedWords = words.map(word => {
+        const firstLetter = word.charAt(0).toUpperCase();
+        const restOfWord = word.slice(1);
+        return firstLetter + restOfWord;
+    });
+    const capitalizedString = capitalizedWords.join(' ');
+    return capitalizedString;
+}
 
-// const capitalize = 'the quick brown fox';
-// const output = capitalizeFirstLetter(capitalize);
-// console.log(output);
+const capitalize = 'the quick brown fox';
+const output = capitalizeFirstLetter(capitalize);
+console.log(output);
 
-// function findLongestWord(str) {
-//     var words = str.split(" ");
+function findLongestWord(str) {
+    var words = str.split(" ");
 
-//     var longestWord = "";
-//     var maxLength = 0;
+    var longestWord = "";
+    var maxLength = 0;
 
-//     for (var i = 0; i < words.length; i++) {
-//         var currentWord = words[i];
-//         if (currentWord.length > maxLength) {
-//             maxLength = currentWord.length;
-//             longestWord = currentWord;
-//         }
-//     }
-//     return longestWord;
-// }
+    for (var i = 0; i < words.length; i++) {
+        var currentWord = words[i];
+        if (currentWord.length > maxLength) {
+            maxLength = currentWord.length;
+            longestWord = currentWord;
+        }
+    }
+    return longestWord;
+}
 
-// var inputString = 'Web Development Tutorial';
-// var longestWord = findLongestWord(inputString);
-// document.write(longestWord);
+var inputString = 'Web Development Tutorial';
+var longestWord = findLongestWord(inputString);
+document.write(longestWord);
 
-// function findLongestWord(str) {
-//     var words = str.split(" ");
+function findLongestWord(str) {
+    var words = str.split(" ");
 
-//     var longestWord = "";
-//     var maxLength = 0;
+    var longestWord = "";
+    var maxLength = 0;
 
-//     for (let i = 0; i < words.length; i++) {
-//         var currentWord = words[i];
-//         if (currentWord.length > maxLength) {
-//             maxLength = currentWord.length;
-//             longestWord = currentWord;
-//         }
+    for (let i = 0; i < words.length; i++) {
+        var currentWord = words[i];
+        if (currentWord.length > maxLength) {
+            maxLength = currentWord.length;
+            longestWord = currentWord;
+        }
 
-//     }
-//     return longestWord;
-// }
-// var charString = prompt("Enter Your Longest Character");
-// var longestChar = findLongestWord(charString);
-// document.write(longestChar);
+    }
+    return longestWord;
+}
+var charString = prompt("Enter Your Longest Character");
+var longestChar = findLongestWord(charString);
+document.write(longestChar);
 
-// function countLetter(string, letter) {
-//     let count = 0;
-//     for (let i = 0; i < string.length; i++) {
-//         if (string.charAt(i) === letter) {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-// var CountLetter = countLetter("JSResourceS.com", "o");
-// document.write(CountLetter);
+function countLetter(string, letter) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+var CountLetter = countLetter("JSResourceS.com", "o");
+document.write(CountLetter);
 
-// function calcCircumference(radius) {
-//     var circumference = 2 * Math.PI * radius;
-//     console.log("Your Value Is " + circumference.toFixed(2));
-// }
+function calcCircumference(radius) {
+    var circumference = 2 * Math.PI * radius;
+    console.log("Your Value Is " + circumference.toFixed(2));
+}
 
-// function calArea(radius) {
-//     var area = Math.PI * Math.pow(radius, 2);
-//     console.log("The Area Is " + area.toFixed(2));
-// }
+function calArea(radius) {
+    var area = Math.PI * Math.pow(radius, 2);
+    console.log("The Area Is " + area.toFixed(2));
+}
 
-// calcCircumference(5);
-// calArea(5);
+calcCircumference(5);
+calArea(5);
